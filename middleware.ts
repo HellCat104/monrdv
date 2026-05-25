1ee073886792ca4717335bee05ee9ba955741d91
HellCat104 <asmaadouach@gmail.com>
2026-05-25 16:06:08 +0200

    Fix TypeScript error in middleware cookies typing

--- middleware.ts
+++ middleware.ts
@@ -56,9 +56,9 @@ export async function middleware(req: NextRequest) {
       {
         cookies: {
           getAll: () => req.cookies.getAll(),
-          setAll: (cookiesToSet) => {
+          setAll: (cookiesToSet: { name: string; value: string; options?: Record<string, unknown> }[]) => {
             cookiesToSet.forEach(({ name, value, options }) => {
-              res.cookies.set(name, value, options)
+              res.cookies.set(name, value, options as Parameters<typeof res.cookies.set>[2])
             })
           },
         },
